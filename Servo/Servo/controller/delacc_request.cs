deffd0b5842d69128eb6491b17f5df13c0f1b6da
Noisec <70171738+Noisec@users.noreply.github.com>
2025-12-01 11:35:08 +0100

    Add files via upload

---
+++ Servo/Servo/controller/delacc_request.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servo.controller
+{
+    internal class delacc_request
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void main(HttpListenerContext data, string lenyeg)
+        {
+            string kert = data.Request.Url.AbsolutePath.TrimStart('/');
+
+            
+            
+            
+
+          
+            string id = "";
+            string jelszo = "";
+
+            try
+            {
+                
+                using (StreamReader reader = new StreamReader(data.Request.InputStream, data.Request.ContentEncoding))
+                {
+                    lenyeg = reader.ReadToEnd();
+                }
+
+                service.shared.log($"Body received: {lenyeg}");
+
+                try { 
+                JObject jsonObj = JObject.Parse(lenyeg);
+                id = service.shared.b64dec(jsonObj["id"].ToString());
+                jelszo = service.shared.b64dec(jsonObj["password"].ToString());
+                }
+                catch
+                {
+
+                    data.Response.StatusCode = 400;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_request");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                    return;
+                }
+
+
+                service.shared.log("Account deletion request: " + id + "  " + jelszo + "  (" + data.Request.RemoteEndPoint.Address.ToString());
+
+                int resp = service.delacc_request.main(id, jelszo, data.Request.RemoteEndPoint.Address.ToString());
+                if (resp == 200)
+                {
+                    data.Response.StatusCode = 200;
+                    byte[] buffer = Encoding.UTF8.GetBytes("email_kuldve");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else if (resp == 401)
+                {
+                    data.Response.StatusCode = 401;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_jelszo");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else //if (resp == 500)
+                {
+                    data.Response.StatusCode = 500;
+                    byte[] buffer = Encoding.UTF8.GetBytes("sze_v_felhasznalo_nem_letezik");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                service.shared.log($"Error: {ex.Message}");
+                data.Response.StatusCode = 400;
+                byte[] buffer = Encoding.UTF8.GetBytes("Hibas request");
+                data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            finally
+            {
+                data.Response.OutputStream.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Servo/Servo/model/registration_request.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Ocsp;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servo.model
+{
+    internal class registration_request
+    {
+        static string connStr = model.shared.connStr;
+        static MySqlConnection conn = model.shared.conn;
+        public static Boolean mainn(Dictionary<string, string> lista = null)
+        {
+            Boolean ret = false;
+            using (MySqlCommand cmd = new MySqlCommand("create_account", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@p_email", lista["p_email"]);
+                cmd.Parameters.AddWithValue("@p_sesstoken", lista["p_sesstoken"]);
+                cmd.Parameters.AddWithValue("@p_passhash", lista["p_passhash"]);
+                cmd.Parameters.AddWithValue("@p_sesstoken_expire", lista["p_sesstoken_expire"]);
+                cmd.Parameters.AddWithValue("@p_first_name", lista["p_first_name"]);
+                cmd.Parameters.AddWithValue("@p_last_name", lista["p_last_name"]);
+                cmd.Parameters.AddWithValue("@p_account_state", lista["p_account_state"]);
+                cmd.ExecuteNonQuery();
+
+                ret = true;
+            }
+            return ret;
+        }
+
+
+    }
+}
