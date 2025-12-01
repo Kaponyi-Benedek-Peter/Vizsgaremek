3d9dae2e3be0bba03c1bbd5a713ab860df8985ab
Noisec <noisec@protonmail.com>
2025-12-01 14:09:51 +0100

    mappa fix

---
+++ backend/Servo/controller/delacc_promise.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servo.controller
+{
+    internal class delacc_promise
+    {
+
+
+        public static void main(HttpListenerContext data, string lenyeg)
+        {
+            string kert = data.Request.Url.AbsolutePath.TrimStart('/');
+
+            
+            
+            
+
+
+            string id = "";
+            string confirmation_token = "";
+
+            try
+            {
+                
+                using (StreamReader reader = new StreamReader(data.Request.InputStream, data.Request.ContentEncoding))
+                {
+                    lenyeg = reader.ReadToEnd();
+                }
+
+                service.shared.log($"Body received: {lenyeg}");
+
+                
+                JObject jsonObj = JObject.Parse(lenyeg);
+
+                try { 
+                
+
+                
+                id = service.shared.b64dec(jsonObj["id"].ToString());
+
+                //error
+                confirmation_token = service.shared.b64dec(jsonObj["token"].ToString());
+
+                }
+                catch
+                {
+
+                    data.Response.StatusCode = 400;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_request");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                    return;
+                }
+
+
+
+
+                service.shared.log("Account deletion request: " + id + "  " + confirmation_token + "  (" + data.Request.RemoteEndPoint.Address.ToString());
+
+                int resp = service.delacc_promise.main(id, confirmation_token, data.Request.RemoteEndPoint.Address.ToString());
+                if (resp == 200)
+                {
+                    data.Response.StatusCode = 200;
+                    byte[] buffer = Encoding.UTF8.GetBytes("felhasznalo_torolve");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else if (resp == 401)
+                {
+                    data.Response.StatusCode = 401;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_token");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else //if (resp == 500)
+                {
+                    data.Response.StatusCode = 500;
+                    byte[] buffer = Encoding.UTF8.GetBytes("szerver_hiba_vagy_felhasznalo_nem_letezik");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                service.shared.log($"Error: {ex.Message}");
+                data.Response.StatusCode = 400;
+                byte[] buffer = Encoding.UTF8.GetBytes("hibas_request");
+                data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            finally
+            {
+                data.Response.OutputStream.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ backend/Servo/controller/delacc_request.cs
@@ -0,0 +1,182 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servo.controller
+{
+    internal class delacc_request
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void main(HttpListenerContext data, string lenyeg)
+        {
+            string kert = data.Request.Url.AbsolutePath.TrimStart('/');
+
+            
+            
+            
+
+          
+            string id = "";
+            string jelszo = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            try
+            {
+                
+                using (StreamReader reader = new StreamReader(data.Request.InputStream, data.Request.ContentEncoding))
+                {
+                    lenyeg = reader.ReadToEnd();
+                }
+
+                service.shared.log($"Body received: {lenyeg}");
+
+                try { 
+                JObject jsonObj = JObject.Parse(lenyeg);
+                id = service.shared.b64dec(jsonObj["id"].ToString());
+                jelszo = service.shared.b64dec(jsonObj["password"].ToString());
+                }
+                catch
+                {
+
+                    data.Response.StatusCode = 400;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_request");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                    return;
+                }
+
+                int resp = 0;
+                service.shared.log("Account deletion request: " + id + "  " + jelszo + "  (" + data.Request.RemoteEndPoint.Address.ToString());
+                try
+                {
+                    service.shared.log("anyád stáció -1");
+                    resp = service.delacc_request.main(id, jelszo, data.Request.RemoteEndPoint.Address.ToString());
+                    service.shared.log("anyád stáció +1");
+                }
+                catch (Exception ex)
+                {
+                    service.shared.log(ex.Message + " -controller.delacc_request.main line 101");
+                }
+
+                if (resp == 200)
+                {
+                    data.Response.StatusCode = 200;
+                    byte[] buffer = Encoding.UTF8.GetBytes("email_kuldve");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else if (resp == 401)
+                {
+                    data.Response.StatusCode = 401;
+                    byte[] buffer = Encoding.UTF8.GetBytes("hibas_jelszo");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+                else //if (resp == 500)
+                {
+                    data.Response.StatusCode = 500;
+                    byte[] buffer = Encoding.UTF8.GetBytes("sze_v_felhasznalo_nem_letezik");
+                    data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            catch (Exception ex)
+            {
+                service.shared.log($"Error: {ex.Message}");
+                data.Response.StatusCode = 400;
+                byte[] buffer = Encoding.UTF8.GetBytes("Hibas request"); // magyarul: bárhol lehet a hiba
+                data.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            finally
+            {
+                data.Response.OutputStream.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
