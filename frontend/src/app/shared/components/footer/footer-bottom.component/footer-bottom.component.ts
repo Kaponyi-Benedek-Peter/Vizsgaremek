224de48666f79e192266ea7231927e315c0731fd
Kaponyi-Benedek-Peter <bp.kaponyi@gmail.com>
2026-01-07 14:10:02 +0100

    Projekt átírása Angularba

---
+++ frontend/src/app/shared/components/footer/footer-bottom.component/footer-bottom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-bottom.component',
+  imports: [],
+  templateUrl: './footer-bottom.component.html',
+  styleUrl: './footer-bottom.component.css',
+})
+export class FooterBottomComponent {
+
+}
