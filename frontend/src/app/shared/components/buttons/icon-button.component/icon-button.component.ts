f68f9bc17a5534d4099042ccbb7cd2266d187353
Kaponyi-Benedek-Peter <bp.kaponyi@gmail.com>
2026-01-09 09:15:52 +0100

    creating components for buttons

---
+++ frontend/src/app/shared/components/buttons/icon-button.component/icon-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-button.component',
+  imports: [],
+  templateUrl: './icon-button.component.html',
+  styleUrl: './icon-button.component.css',
+})
+export class IconButtonComponent {
+
+}
