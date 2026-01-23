4824542a7e37b980976ebfacdc55be7673883ff3
Kaponyi-Benedek-Peter <bp.kaponyi@gmail.com>
2026-01-23 11:41:32 +0100

    reworks, preparing resources for search bar

--- frontend/src/app/shared/components/footer/footer-bottom/footer-bottom.ts
+++ frontend/src/app/shared/components/footer/footer-bottom/footer-bottom.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { IconBtn } from '../../icon-btn/icon-btn';
+import { ICONS, IMAGES } from '../../../../core/constants/visuals';
+import { PrimaryBtn } from '../../primary-btn/primary-btn';
 
 @Component({
   selector: 'app-footer-bottom',
   standalone: true,
-  imports: [IconBtn],
+  imports: [IconBtn, PrimaryBtn],
   templateUrl: './footer-bottom.html',
   styleUrl: './footer-bottom.css',
 })
-export class FooterBottom {}
+export class FooterBottom {
+  ICONS = ICONS;
+  IMAGES = IMAGES;
+}
