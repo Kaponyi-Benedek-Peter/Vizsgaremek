4e95418fbf44cff442d8d40809ddb7ebe0424081
Kaponyi-Benedek-Peter <bp.kaponyi@gmail.com>
2026-04-29 18:41:33 +0200

    cleaning main from codes, making space for docs, and more

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseURL: 'https://www.roysshack.hu',
-  assetsURL: '',
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  baseURL: 'https://www.roysshack.hu',
-  assetsURL: '',
-};
